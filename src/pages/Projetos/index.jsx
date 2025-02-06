ff4d21b06e037f2d69d7e2076113f91c006cd6b7
Joao <joaomarquesvictor22@hotmail.com>
2025-02-06 18:08:21 -0300

    Adicionada base da página projetos

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -1,9 +1,23 @@
 import styles from "./Projetos.module.css"
+import { FaGithub } from "react-icons/fa";
 
 function Projetos () {
 
     return (
-            <h1>Projetos</h1>
+            <section className={styles.projetos}>
+                <div className={styles.cardProjeto}>
+                    <div className={styles.titulos}>
+                        <h1 className={styles.titulo}>NAStock</h1>
+                        <h2 className={styles.areaDev}>FullStack</h2>
+                    </div>
+                    <img/>
+                    <h3>Projeto desenvolvido com a intenção de controlar o estoque interno de equipamentos e patrimônios da empresa, disponibilizando sobre eles informações do hardware, local onde se encontra e diversas observações. Tudo isso criado com as tecnologias mais modernas de desenvolvimento como <strong>React, NodeJS, Express</strong> armazenando seus dados em um banco <strong>SQLite</strong>. </h3>
+                    <div className={styles.botoes}> 
+                        <a className={styles.botao}>Saiba mais</a>
+                        <a className={styles.botao} href="https://github.com/joaovictormar/NAStock_front"><FaGithub/></a>
+                    </div>
+                </div>
+            </section>
     )
 
 };
