c82ec0ab922478a5b09ed4041bdbc155e95066d8
Joao <joaomarquesvictor22@hotmail.com>
2025-02-06 18:28:25 -0300

    Feita aletrações no currículo da página resumo

--- src/pages/Resumo/index.jsx
+++ src/pages/Resumo/index.jsx
@@ -1,15 +1,15 @@
 import styles from "./Resumo.module.css"
 import curriculo from "../../assets/curriculo.png"
-import curriculoPDF from "../../assets/curriculo.pdf"
+import curriculoPDF from "../../assets/Curriculo Joao Victor.pdf"
 
 
 function Resumo () {
 
     return (
             <section className={styles.resumo}>
-                <a className={styles.botao} href={curriculoPDF} donwload>Download Currículo</a>
+                <a className={styles.botao} href={curriculoPDF} download>Download Currículo</a>
                 <img className={styles.curriculo} src={curriculo}/> 
-                <a className={styles.botao} href={curriculoPDF} donwload>Download Currículo</a>
+                <a className={styles.botao} href={curriculoPDF} download>Download Currículo</a>
             </section>
     )
 
