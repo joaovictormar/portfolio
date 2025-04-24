c392969535fe5dbc01808ea195143d4320e4d566
João Victor <joaomarquesvictor22@hotmail.com>
2025-04-24 09:58:40 -0300

    Realizada alterações na seção de projetos

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -1,6 +1,6 @@
 import styles from "./Projetos.module.css"
 import { FaGithub } from "react-icons/fa";
-import nastock from "../../assets/NAStock.png"
+import nastock from "../../assets/NASITstock.png"
 
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
@@ -21,7 +21,7 @@ function Projetos () {
                         <h2 className={styles.areaDev}>FullStack</h2>
                     </div>
                     <img className={styles.imagem} src={nastock}/>
-                    <h3>Projeto desenvolvido com a intenção de ser um sistema de controle de estoque interno de equipamentos e patrimônios da empresa, disponibilizando sobre eles informações do hardware, local onde se encontram e diversas observações. Tudo isso disponibilizado em um ambiente web, onde o usuário deve realizar a autenticação antes de utilizar o sistema e se o mesmo for um usuário permitido, manipular os equipamentos. O projeto foi criado com as tecnologias mais modernas de desenvolvimento como <strong>React, NodeJS, Express</strong> armazenando seus dados em um banco <strong>SQLite</strong>.</h3>
+                    <h3>Projeto desenvolvido com a intenção de ser um sistema de controle de estoque interno de equipamentos da empresa, disponibilizando sobre eles informações relevantes do hardware, local onde se encontram e outras observações. Tudo isso disponibilizado em um ambiente web, onde o usuário deve realizar a autenticação antes de utilizar o sistema e se o mesmo for um usuário permitido, manipular os equipamentos. O projeto foi criado com as tecnologias mais modernas de desenvolvimento como <strong>React, NodeJS, Express</strong> armazenando seus dados em um banco <strong>SQLite</strong>.</h3>
                     <div className={styles.botoes}> 
                         <a className={styles.botao}>Saiba mais</a>
                         <a className={styles.botao} href="https://github.com/joaovictormar/NAStock_front"><FaGithub/></a>
